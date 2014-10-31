b8c307304645e47e959e8bf26d8452dbf0ecd985
Will Riazati <wriazati@gmail.com>
2014-10-30 20:03:05 -0700

    adding another test

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -77,6 +77,64 @@ void CreateAThread(int n)
 }
 
 
+void
+Joiner(Thread *joinee)
+{
+  currentThread->Yield();
+  currentThread->Yield();
+
+  printf("Waiting for the Joinee to finish executing.\n");
+
+  currentThread->Yield();
+  currentThread->Yield();
+
+  // Note that, in this program, the "joinee" has not finished
+  // when the "joiner" calls Join().  You will also need to handle
+  // and test for the case when the "joinee" _has_ finished when
+  // the "joiner" calls Join().
+
+  joinee->Join();
+
+  currentThread->Yield();
+  currentThread->Yield();
+
+  printf("Joinee has finished executing, we can continue.\n");
+
+  currentThread->Yield();
+  currentThread->Yield();
+}
+
+void
+Joinee()
+{
+  int i;
+
+  for (i = 0; i < 5; i++) {
+    printf("Smell the roses.\n");
+    currentThread->Yield();
+  }
+
+  currentThread->Yield();
+  printf("Done smelling the roses!\n");
+  currentThread->Yield();
+}
+
+void
+ForkerThread()
+{
+  Thread *joiner = new Thread("joiner");  // will not be joined
+  Thread *joinee = new Thread("joinee", 1);  // WILL be joined
+
+  // fork off the two threads and let them do their business
+  joiner->Fork((VoidFunctionPtr) Joiner, (int) joinee);
+  joinee->Fork((VoidFunctionPtr) Joinee, 0);
+
+  // this thread is done and can go on its merry way
+  printf("Forked off the joiner and joiner threads.\n");
+}
+
+
+
 //----------------------------------------------------------------------
 // ThreadTest1
 // 	Set up a ping-pong between two threads, by forking a thread
@@ -107,6 +165,7 @@ void ThreadTest100()
 	printf("%s", "--------------------------------\n");
 }
 
+// Child finishes before parent calls join
 void ThreadTest101()
 {
 	printf("%s", "--------------------------------\n");
@@ -144,7 +203,6 @@ void ThreadTest102()
 
 
 
-
 //----------------------------------------------------------------------
 // ThreadTest
 // 	Invoke a test routine.
@@ -165,6 +223,9 @@ ThreadTest()
 	break;
     case 102:
     	ThreadTest102();
+	break;
+    case 103:
+    	ForkerThread();
 	break;
     default:
         printf("No test specified.\n");
